cd95f5c8e2e00a5abec0d0492bd94314d1afdda6
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-08-04 20:30:57 +0300

    added 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+
+
+function createMarkupGallery(arr) {
+    return arr.map(({ preview, original, description }) =>`
+  <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+     </a>
+  </li>`).join('')
+}
+
+const galleryMarkup = createMarkupGallery(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+// galleryContainer.addEventListener('click', onImageClick)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
+// console.log(lightbox);
