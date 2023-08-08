35df468888c372dc9345c4e55f0602bfb61d1052
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-08-08 16:18:59 +0300

    added 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,46 @@
 import throttle from 'lodash.throttle';
 
-// const formFeedback = document.querySelector('.feedback-form')
-// // console.dir(formFeedback);
+const formFeedback = document.querySelector('.feedback-form')
+// console.dir(formFeedback);
 
-// formFeedback.addEventListener('input', onInput)
-// formFeedback.addEventListener('submit', onSubmit)
+formFeedback.addEventListener('input', throttle(onInput, 500));
+formFeedback.addEventListener('submit', onSubmit);
 
-// const LOCALSTORAGE_KEY = "feedback-form-state";
+const LOCALSTORAGE_KEY = "feedback-form-state";
 
-// function onInput(evt) {
-// localStorage.getItem(LOCALSTORAGE_KEY)
-// }
+let feedbackFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+console.log(feedbackFormData);
+const { email, message } = formFeedback.elements;
+console.log(formFeedback.elements);
 
-// function onSubmit(evt) {
-//     evt.preventDefault();
-//     const { email, message } = evt.currentTarget.elements;
-//     if (email.value === '' || message.value === '') {
-//         return alert('Please fill in all the fields!');
-//     } 
-//     const object = {
-//             email: email.value,
-//             password: message.value,
-//         }
-//     console.log(object);
+reload();
+
+function onInput(evt) {
+    feedbackFormData = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormData));
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+   
+    if (email.value === '' || message.value === '') {
+        return alert('Please fill in all the fields!');
+    } 
+    const object = {
+            email: email.value,
+            password: message.value,
+        }
+    console.log(object);
     
-//     // localStorage.removeItem(LOCALSTORAGE_KEY)
-//     evt.currentTarget.reset();
-// }
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+    evt.currentTarget.reset();
+}
 
+
+function reload() {
+if (feedbackFormData) {
+    email.value = feedbackFormData.email || '';
+    message.value = feedbackFormData.message || '';
+  }
+}
 
