cd895da8a2006a4a123e7f2df2baef0a3351e9dc
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-08-08 15:35:19 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+
+// const formFeedback = document.querySelector('.feedback-form')
+// // console.dir(formFeedback);
+
+// formFeedback.addEventListener('input', onInput)
+// formFeedback.addEventListener('submit', onSubmit)
+
+// const LOCALSTORAGE_KEY = "feedback-form-state";
+
+// function onInput(evt) {
+// localStorage.getItem(LOCALSTORAGE_KEY)
+// }
+
+// function onSubmit(evt) {
+//     evt.preventDefault();
+//     const { email, message } = evt.currentTarget.elements;
+//     if (email.value === '' || message.value === '') {
+//         return alert('Please fill in all the fields!');
+//     } 
+//     const object = {
+//             email: email.value,
+//             password: message.value,
+//         }
+//     console.log(object);
+    
+//     // localStorage.removeItem(LOCALSTORAGE_KEY)
+//     evt.currentTarget.reset();
+// }
+
 
